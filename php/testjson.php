b625088436cdc530acb8c60b283af6cdf09b9e4e
EvripidisPa <evrispa@gmail.com>
2019-12-18 16:33:45 +0200

    Added json sample files,basic json parser and gitignore

---
+++ php/testjson.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    Ohh wee
+    <pre>
+      <?php
+      $json = file_get_contents("../0_Demo_data/sample.json");
+      $json2 = json_decode($json,TRUE);
+      print_r($json2);
+
+      echo "<h2>Printing activities example</h2>\n";
+      foreach ($json2['activity'] as $act) {
+        print_r($act);
+      }
+
+      /** Another way to parse
+        * Personally not really prefered
+        */
+      $jsonIterator = new RecursiveIteratorIterator(
+      new RecursiveArrayIterator(json_decode($json, TRUE)),
+      RecursiveIteratorIterator::SELF_FIRST);
+
+      foreach ($jsonIterator as $key => $val) {
+          if(is_array($val)) {
+              echo "key : $key:\n";
+          } else {
+              echo "key==> val : $key => $val\n";
+          }
+      }
+
+      ?>
+    </pre>
+  </body>
+</html>
