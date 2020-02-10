e550c449f55f8fb850853e075d2c21b2b94664b5
MariosKarmalis <45511629+MariosKarmalis@users.noreply.github.com>
2020-02-10 21:02:17 +0200

    Dashboard Initiative Files + Complete Login
    Dashboard Initiative Files ( One for Users  && One for admins )+ Complete Login Functional with Database interaction.

---
+++ php/admin.php
@@ -0,0 +1,64 @@
+<?php  
+ $connect = mysqli_connect("localhost", "root", "", "mydb");  
+ session_start();  
+ if(isset($_SESSION["username"]))  
+ {  
+      header("location:welcome_admin.php");  
+ }  
+ 
+ if( $_SERVER['REQUEST_METHOD'] == 'POST'  )
+ {  
+      if(empty($_POST["username"]) && empty($_POST["password"]))  
+      {  
+           echo '<script>alert("Both Fields are required")</script>';  
+      }  
+      else  
+      {  
+           $username = mysqli_real_escape_string($connect, $_POST["username"]);  
+           $password = mysqli_real_escape_string($connect, $_POST["password"]);  
+           //$password = md5($password);  
+           $query = "SELECT * FROM admins WHERE username = '$username' AND password = '$password'";  
+           $result = mysqli_query($connect, $query);  
+           if(mysqli_num_rows($result) > 0)  
+           {  
+                $_SESSION['username'] = $username;  
+                header("location:welcome_admin.php");  
+           }  
+           else  
+           {  
+                echo '<script>alert("Wrong User Details")</script>';  
+           }  
+      }  
+ }  
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+		
+      <head >  
+           <title> Admin Dashboard Login </title>  
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+      </head>  
+      <body style="background-image: url('font.jpg');">  
+           <br /><br />  
+           <div class="container" style="width:500px; ">  
+                <h3 align="center">Admin Dashboard Login</h3>  
+                <br />  
+                <h3 align="center"></h3>  
+                <br />  
+                <form method="post">  
+                     <label>Enter Username</label>  
+                     <input type="text" name="username" class="form-control" />  
+                     <br />  
+                     <label>Enter Password</label>  
+                     <input type="password" name="password" class="form-control" />  
+                     <br />
+                     <input type="submit" name="admin login" value="Login" class="btn btn-info" />  
+                     <br />  
+					 <p align="right"><a href="index.php?action=login">User Login</a></p>  
+					 <p align="right"><a href="index.php">User Register</a></p>  
+                </form>   
+           </div>  
+      </body>  
+ </html>  
--- php/index.php
+++ php/index.php
@@ -1,11 +1,36 @@
-<?php  
+<?php   
  $connect = mysqli_connect("localhost", "root", "", "mydb");  
  session_start();  
  if(isset($_SESSION["username"]))  
  {  
       header("location:welcome.php");  
  }  
- if(isset($_POST["register"]))  
+ 
+ if(isset($_POST["login"]))  
+ {  
+      if(empty($_POST["username"]) && empty($_POST["password"]))  
+      {  
+           echo '<script>alert("Both Fields are required")</script>';  
+      }  
+      else  
+      {  
+           $username = mysqli_real_escape_string($connect, $_POST["username"]);  
+           $password = mysqli_real_escape_string($connect, $_POST["password"]);  
+           $password = md5($password);  
+           $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";  
+           $result = mysqli_query($connect, $query);  
+           if(mysqli_num_rows($result) > 0)  
+           {  
+                $_SESSION['username'] = $username;  
+                header("location:welcome.php");  
+           }  
+           else  
+           {  
+                echo '<script>alert("Wrong User Details")</script>';  
+           }  
+      }  
+ } 
+ if(isset($_POST["register"])) 
  {
 	  $ucl = preg_match('/[a-zA-Z]/', $_POST["password"]); // Uppercase Letter
 	  $dig = preg_match('/\d/', $_POST["password"]); // Numeral
@@ -41,51 +66,27 @@
                 echo '<script>alert("Registration Done")</script>';  
            }  
       }  
-	  
- }  
- if(isset($_POST["login"]))  
- {  
-      if(empty($_POST["username"]) && empty($_POST["password"]))  
-      {  
-           echo '<script>alert("Both Fields are required")</script>';  
-      }  
-      else  
-      {  
-           $username = mysqli_real_escape_string($connect, $_POST["username"]);  
-           $password = mysqli_real_escape_string($connect, $_POST["password"]);  
-           $password = md5($password);  
-           $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";  
-           $result = mysqli_query($connect, $query);  
-           if(mysqli_num_rows($result) > 0)  
-           {  
-                $_SESSION['username'] = $username;  
-                header("location:welcome.php");  
-           }  
-           else  
-           {  
-                echo '<script>alert("Wrong User Details")</script>';  
-           }  
-      }  
- }  
- ?>  
+ } 
+ ?> 
+ 
  <!DOCTYPE html>  
  <html>  
-      <head>  
-           <title> PHP Login Registration Form with md5() Password Encryption</title>  
+		
+      <head >  
+           <title>User Login</title>  
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
       </head>  
-      <body>  
+      <body style="background-image: url('font.jpg');">  
            <br /><br />  
-           <div class="container" style="width:500px;">  
-                <h3 align="center">PHP Login Registration Form with md5() Password Encryption</h3>  
+           <div class="container" style="width:500px; ">  
                 <br />  
                 <?php  
-                if(isset($_GET["action"]) == "login")  
+                if (isset($_GET["action"]) == "login")  
                 {  
                 ?>  
-                <h3 align="center">Login Form</h3>  
+                <h3 align="center">User Login</h3>  
                 <br />  
                 <form method="post">  
                      <label>Enter Username</label>  
@@ -96,15 +97,16 @@
                      <br />  
                      <input type="submit" name="login" value="Login" class="btn btn-info" />  
                      <br />  
-                     <p align="center"><a href="index.php">Register</a></p>  
+                     <p align="right"><a href="index.php">Register User</a></p>  
+                     <p align="right"><a href="admin.php">Admin Login</a></p>  
                 </form>  
-                <?php       
+                <?php
                 }  
-                else  
-                {  
+                else 
+                {
                 ?>  
-                <h3 align="center">Register Form</h3>  
-                <br />  
+				<h3 align="center">Register User</h3>  
+				<br />  
                 <form method="post">  
                      <label>Enter Username</label>  
                      <input type="text" name="username" class="form-control" />  
@@ -117,11 +119,12 @@
 					 <br />
                      <input type="submit" name="register" value="Register" class="btn btn-info" />  
                      <br />  
-                     <p align="center"><a href="index.php?action=login">Login</a></p>  
+                     <p align="right"><a href="index.php?action=login">User Login</a></p>  
+                     <p align="right"><a href="admin.php">Admin Login</a></p>  
                 </form>  
                 <?php  
-                }  
-                ?>  
+                }
+				?>
            </div>  
       </body>  
- </html>  
+ </html>
