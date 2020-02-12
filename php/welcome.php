3ee370aac4871a8afaa95736715585d7362a7034
MariosKarmalis <45511629+MariosKarmalis@users.noreply.github.com>
2020-02-12 17:03:21 +0200

    Add files via upload
    Update of welcome.php and welcome_admin.php

--- php/welcome.php
+++ php/welcome.php
@@ -3,24 +3,28 @@
   if(!isset($_SESSION["username"]))  
   {  
     header("location:index.php?action=login");  
-  }  
+  }
+    
 ?>
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <link rel="title icon" href="images/title-img.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>User Dashboard</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
     
     <!-- navbar -->
-    <nav class="navbar navbar-expand-md navbar-light">
+    <nav class="navbar navbar-expand-md navbar-light" style="%">
       <button class="navbar-toggler ml-auto mb-2 bg-light" type="button" data-toggle="collapse" data-target="#myNavbar">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -29,14 +33,14 @@
           <div class="row">
             <!-- sidebar -->
             <div class="col-xl-2 col-lg-3 col-md-4 sidebar fixed-top">
-              <a href="#" class="navbar-brand text-white d-block mx-auto text-center py-3 mb-4 bottom-border">Admin Dashboard UI</a>
+              <a href="#" class="navbar-brand text-white d-block mx-auto text-center py-3 mb-4 bottom-border">User Dashboard UI</a>
               <div class="bottom-border pb-3">
                 
-                <a href="#" class="text-white"><?php echo $_SESSION["username"]; ?></a>
+                <a href="#" class="text-white"><?php echo $_SESSION["username"];  ?></a>
                 
               </div>
               <ul class="navbar-nav flex-column mt-4">
-                <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 current"><i class="fas fa-home text-light fa-lg mr-3"></i>Dashboard</a></li>
+                <!-- <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 current"><i class="fas fa-home text-light fa-lg mr-3"></i>Dashboard</a></li> -->
                 <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fas fa-chart-line text-light fa-lg mr-3"></i>Analytics</a></li>
                 <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fas fa-chart-bar text-light fa-lg mr-3"></i>Heatmap</a></li>
                 <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fas fa-table text-light fa-lg mr-3"></i>Tables</a></li>
@@ -45,10 +49,17 @@
               </ul>
             </div>
             <!-- end of sidebar -->
-
+        
             <!-- top-nav -->
-            <div class="col-lg-9">
-             
+            <div class="col-sm-12 d-flex justify-content-end">
+            <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap  p-0">
+              <!--<input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
+              <ul class="navbar-nav px-3">
+              <li class="nav-item text-nowrap">
+                <a class="nav-link" href="logout.php">Logout</a>
+              </li>
+      </ul>
+    </nav>
             </div>
             <!-- end of top-nav -->
           </div>
@@ -57,29 +68,23 @@
     </nav>
     <!-- end of navbar -->
    
-     <!-- modal -->
-    <div class="modal fade" id="sign-out">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h4 class="modal-title">Want to leave?</h4>
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-          </div>
-          <div class="modal-body">
-            Press logout to leave
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-success" data-dismiss="modal">Stay Here</button>
-            <button type="button" class="btn btn-danger" data-dismiss="modal">Logout <a href="logout.php"> </button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- end of modal -->
+  <div class="container1">
+  <p> Select Date Below </p>  
+  <input id="input" width="196" > </input>
+    <script type=text/javascript>
+        $('#input').datetimepicker({ footer: true, modal: true });
+    </script>
+    
+  </div> 
+    
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
     <script src="script.js"></script>
+  
+    
+
   </body>
 </html>
 
--- php/welcome_admin.php
+++ php/welcome_admin.php
@@ -5,6 +5,7 @@
     header("location:index.php?action=login");  
   }  
 ?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -46,9 +47,17 @@
             </div>
             <!-- end of sidebar -->
 
-            <!-- top-nav -->
-            <div class="col-lg-9">
-             
+           <!-- top-nav -->
+           <div class="col-sm-9 d-flex justify-content-end">
+            <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap  p-0">
+              <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+              <ul class="navbar-nav px-3">
+              <li class="nav-item text-nowrap">
+                <a class="nav-link" href="logout.php">Logout</a>
+              </li>
+      </ul>
+    </nav>
+            </div>
             </div>
             <!-- end of top-nav -->
           </div>
@@ -56,7 +65,15 @@
       </div>
     </nav>
     <!-- end of navbar -->
-   
+    
+  <div class="container1">
+  <p> Select Date Below </p>  
+  <input id="input" width="312" > </input>
+    <script type=text/javascript>
+        $('#input').datetimepicker({ footer: true, modal: true });
+    </script>
+    
+  </div> 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
