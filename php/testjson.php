c7ca4d981fcfeeaf5ff92a2d923cc0ea957e4c4a
EvripidisPa <evrispa@gmail.com>
2020-02-14 00:30:01 +0200

    Added distance function to parser

--- php/testjson.php
+++ php/testjson.php
@@ -12,40 +12,86 @@ include "config.php"
     Ohh wee
     <pre>
       <?php
-      $json = file_get_contents("../0_Demo_data/sample2.json");
-      $json2 = json_decode($json,TRUE);
-        foreach ($json2['locations'] as $loc) {
-          $timestampMs = NULL;
-          $latitudeE7 = NULL;
-          $longitudeE7 = NULL; 
-          $accuracy = "NULL";
-          $velocity = "NULL";
-          $heading = "NULL";
-          $altitude = "NULL";
-          if (array_key_exists('timestampMs', $loc)){
-              $timestampMs = date('Y-m-d h:i:s', (float)$loc['timestampMs'] / 1000);
-            }
-          if (array_key_exists('latitudeE7', $loc)){
-              $latitudeE7 = (float)$loc['latitudeE7']/1e7;
-            }
-          if (array_key_exists('longitudeE7', $loc)){
-              $longitudeE7 = (float)$loc['longitudeE7']/1e7;
-            }
-          if (array_key_exists('accuracy', $loc)){
-              $accuracy = (int)$loc['accuracy'];
-            }
-          if (array_key_exists('velocity', $loc)){
-              $velocity = (int)$loc['velocity'];
-            }
-          if (array_key_exists('heading', $loc)){
-              $heading = (int)$loc['heading'];
-            }
-          if (array_key_exists('altitude', $loc)){
-              $altitude = (int)$loc['altitude'];
-            }
-          
-            // TODO Implement a connection with front-end --> grabbing the user_id from the logged in user --> passing it to the sql insertion. 
 
+      function getDistance($lat,$long)
+      {
+        /**
+         * Function calculating the distance from the point (38.230462,21.753150)
+         * Given 2 params
+         * @lat : float
+         * @long : float
+        */
+
+        $earth_radius = 6371;
+
+        $dLat = deg2rad($lat - 38.230462);  
+        $dLon = deg2rad($long - 21.753150);  
+
+        $a = sin($dLat/2) * sin($dLat/2) + cos(deg2rad(38.230462)) * cos(deg2rad($lat)) * sin($dLon/2) * sin($dLon/2);  
+        $c = 2 * asin(sqrt($a));  
+        $d = $earth_radius * $c;
+
+        return $d;
+      }
+
+      /* 
+      *   JSON FORMAT, LOCATION HISTORY PARSING  
+      *   AND 
+      *   DATABASE STORING
+      *   
+      *   json file structure :
+      *   {locations :[{
+      *     timestampMs,
+      *     latitudeE7,
+      *     longitudeE7,
+      *     accuracy,
+      *     velocity,
+      *     heading,
+      *     altitude,
+      *     activity : [{
+      *       timestampMs,
+      *       activity :[{
+      *         type,confidence
+      *       }]
+      *     }]
+      *   }]
+      */
+
+      $json = file_get_contents("../0_Demo_data/setA.json");
+      $json2 = json_decode($json,TRUE);
+      foreach ($json2['locations'] as $loc) {
+        $timestampMs = NULL;
+        $latitudeE7 = NULL;
+        $longitudeE7 = NULL; 
+        $accuracy = "NULL";
+        $velocity = "NULL";
+        $heading = "NULL";
+        $altitude = "NULL";
+        if (array_key_exists('timestampMs', $loc)){
+            $timestampMs = date('Y-m-d h:i:s', (float)$loc['timestampMs'] / 1000);
+          }
+        if (array_key_exists('latitudeE7', $loc)){
+            $latitudeE7 = (float)$loc['latitudeE7']/1e7;
+          }
+        if (array_key_exists('longitudeE7', $loc)){
+            $longitudeE7 = (float)$loc['longitudeE7']/1e7;
+          }
+        if (array_key_exists('accuracy', $loc)){
+            $accuracy = (int)$loc['accuracy'];
+          }
+        if (array_key_exists('velocity', $loc)){
+            $velocity = (int)$loc['velocity'];
+          }
+        if (array_key_exists('heading', $loc)){
+            $heading = (int)$loc['heading'];
+          }
+        if (array_key_exists('altitude', $loc)){
+            $altitude = (int)$loc['altitude'];
+          }
+          
+          // TODO Implement a connection with front-end --> grabbing the user_id from the logged in user --> passing it to the sql insertion. 
+            
+        if(getDistance($latitudeE7,$longitudeE7 < 10)){
           $sql = "INSERT INTO locations (loc_timestamp,latE7,longE7,accuracy,velocity,altitude,heading,userid) 
             VALUES ('$timestampMs','$latitudeE7','$longitudeE7','$accuracy',$velocity,$heading,$altitude,'5')";
 
@@ -55,6 +101,7 @@ include "config.php"
           } else {
             echo "Error: " . $sql . "<br>" . $link->error;
           }
+          
           if(array_key_exists('activity',$loc)){
             foreach($loc['activity'] as $loc_ac){
               if (array_key_exists('timestampMs', $loc_ac)){
@@ -90,10 +137,10 @@ include "config.php"
                   }
                 }
               }
+            }
           }
         }
-    }
-
+      }
       ?>
     </pre>
   </body>
