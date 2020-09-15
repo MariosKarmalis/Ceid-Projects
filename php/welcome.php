3fd5d3d02a05f284f53fd91f331072d9ab50e848
MariosKarmalis <45511629+MariosKarmalis@users.noreply.github.com>
2020-09-15 21:47:24 +0300

    Implementation of Upload mechanism
    Finalizing the function of upload. TODO: Add some of the activity based scoreboards.

--- php/welcome.php
+++ php/welcome.php
@@ -12,15 +12,15 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <link rel="title icon" href="images/title-img.png">
+    <!-- <link rel="title icon" href="images/title-img.png"> -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
     <title>User Dashboard</title>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
     <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
     
@@ -37,13 +37,10 @@
               <a href="#" class="navbar-brand text-white d-block mx-auto text-center py-3 mb-4 bottom-border">User  UI</a>
               <div class="bottom-border pb-3">
                 
-                <a href="#" class="text-white"><?php echo $_SESSION["username"]; ?></a>
+                <a  class="text-white"><?php echo " User : "; echo $_SESSION["username"];  ?></a>
                 
               </div>
               <ul class="navbar-nav flex-column mt-4">
-                <!-- <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 current"><i class="fas fa-home text-light fa-lg mr-3"></i>Dashboard</a></li> -->
-                <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fas fa-chart-line text-light fa-lg mr-3"></i>Analytics</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fas fa-table text-light fa-lg mr-3"></i>Tables</a></li>
                 <li class="nav-item"><a href="logout.php" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fas  text-light fa-lg mr-3"></i>Logout </a></li>
               </ul>
             </div>
@@ -67,22 +64,23 @@
     </nav>
     <!-- end of navbar -->
    
-  <div class="container1">
-     Select Date: 
-    <input id="input" width="196" > </input>
-    <script type=text/javascript>
-        $('#input').datetimepicker({ footer: true, modal: true });
-    </script>
-  </div> 
-  <div class="container2">
-    <form action="upload.php" method="post" enctype="multipart/form-data">
+
+  <!-- User based functions -->
+
+
+  <!-- DIV for uploading dataset to the database. -->
+  <body class="uploader">
+    <div class="container">
+     <form action="upload.php" method="post" enctype="multipart/form-data">
       Select Data File to Upload:
       
-      <input type="file" name="file">
-      <input type="submit" name="submit" value="Upload">
-    </form>
-  </div>
-
+        <input type="file" name="file">
+       <input type="submit" name="submit" value="Upload">
+       <!-- <button onclick="myFunction()"> Upload</button>  -->
+      </form>
+    </div>
+  </body>
+    
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
@@ -90,9 +88,3 @@
 
   </body>
 </html>
-
-
-
-
-
-
